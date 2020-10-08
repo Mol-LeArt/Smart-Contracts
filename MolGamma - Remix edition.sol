2d3959ade99d0935eb6b4a2f479780be79914315
Stamford Hwang <stamfordh@hotmail.com>
2020-10-08 13:24:57 -0400

    Update MolGamma - Remix edition.sol
    - clean up

--- MolGamma - Remix edition.sol
+++ MolGamma - Remix edition.sol
@@ -1,162 +1,41 @@
-/**
- *Submitted for verification at Etherscan.io on 2020-09-17
+/*
+DEAR MSG.SENDER(S):
+
+/ MolGamma is a project in beta.
+// Please audit and use at your own risk.
+/// There is also a DAO to join if you're curious.
+//// This is code, don't construed this as legal advice or replacement for professional counsel.
+///// STEAL THIS C0D3SL4W
+
+~ presented by Mol LeArt ~
 */
 
 pragma solidity 0.5.17;
 pragma experimental ABIEncoderV2;
 
-/**
- * @dev Wrappers over Solidity's arithmetic operations with added overflow
- * checks.
- *
- * Arithmetic operations in Solidity wrap on overflow. This can easily result
- * in bugs, because programmers usually assume that an overflow raises an
- * error, which is the standard behavior in high level programming languages.
- * `SafeMath` restores this intuition by reverting the transaction when an
- * operation overflows.
- *
- * Using this library instead of the unchecked operations eliminates an entire
- * class of bugs, so it's recommended to use it always.
- */
 library SafeMath {
-    /**
-     * @dev Returns the addition of two unsigned integers, reverting on
-     * overflow.
-     *
-     * Counterpart to Solidity's `+` operator.
-     *
-     * Requirements:
-     * - Addition cannot overflow.
-     */
     function add(uint256 a, uint256 b) internal pure returns (uint256) {
         uint256 c = a + b;
         require(c >= a, "SafeMath: addition overflow");
-
         return c;
     }
-
-    /**
-     * @dev Returns the subtraction of two unsigned integers, reverting on
-     * overflow (when the result is negative).
-     *
-     * Counterpart to Solidity's `-` operator.
-     *
-     * Requirements:
-     * - Subtraction cannot overflow.
-     */
     function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-        return sub(a, b, "SafeMath: subtraction overflow");
-    }
-
-    /**
-     * @dev Returns the subtraction of two unsigned integers, reverting with custom message on
-     * overflow (when the result is negative).
-     *
-     * Counterpart to Solidity's `-` operator.
-     *
-     * Requirements:
-     * - Subtraction cannot overflow.
-     *
-     * _Available since v2.4.0._
-     */
-    function sub(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
-        require(b <= a, errorMessage);
+        require(b <= a);
         uint256 c = a - b;
-
         return c;
     }
-
-    /**
-     * @dev Returns the multiplication of two unsigned integers, reverting on
-     * overflow.
-     *
-     * Counterpart to Solidity's `*` operator.
-     *
-     * Requirements:
-     * - Multiplication cannot overflow.
-     */
     function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-        // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
-        // benefit is lost if 'b' is also tested.
-        // See: https://github.com/OpenZeppelin/openzeppelin-contracts/pull/522
         if (a == 0) {
             return 0;
         }
-
         uint256 c = a * b;
         require(c / a == b, "SafeMath: multiplication overflow");
-
         return c;
     }
-
-    /**
-     * @dev Returns the integer division of two unsigned integers. Reverts on
-     * division by zero. The result is rounded towards zero.
-     *
-     * Counterpart to Solidity's `/` operator. Note: this function uses a
-     * `revert` opcode (which leaves remaining gas untouched) while Solidity
-     * uses an invalid opcode to revert (consuming all remaining gas).
-     *
-     * Requirements:
-     * - The divisor cannot be zero.
-     */
     function div(uint256 a, uint256 b) internal pure returns (uint256) {
-        return div(a, b, "SafeMath: division by zero");
-    }
-
-    /**
-     * @dev Returns the integer division of two unsigned integers. Reverts with custom message on
-     * division by zero. The result is rounded towards zero.
-     *
-     * Counterpart to Solidity's `/` operator. Note: this function uses a
-     * `revert` opcode (which leaves remaining gas untouched) while Solidity
-     * uses an invalid opcode to revert (consuming all remaining gas).
-     *
-     * Requirements:
-     * - The divisor cannot be zero.
-     *
-     * _Available since v2.4.0._
-     */
-    function div(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
-        // Solidity only automatically asserts when dividing by 0
-        require(b > 0, errorMessage);
+        require(b > 0);
         uint256 c = a / b;
-        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-
         return c;
-    }
-
-    /**
-     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
-     * Reverts when dividing by zero.
-     *
-     * Counterpart to Solidity's `%` operator. This function uses a `revert`
-     * opcode (which leaves remaining gas untouched) while Solidity uses an
-     * invalid opcode to revert (consuming all remaining gas).
-     *
-     * Requirements:
-     * - The divisor cannot be zero.
-     */
-    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
-        return mod(a, b, "SafeMath: modulo by zero");
-    }
-
-    /**
-     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
-     * Reverts with custom message when dividing by zero.
-     *
-     * Counterpart to Solidity's `%` operator. This function uses a `revert`
-     * opcode (which leaves remaining gas untouched) while Solidity uses an
-     * invalid opcode to revert (consuming all remaining gas).
-     *
-     * Requirements:
-     * - The divisor cannot be zero.
-     *
-     * _Available since v2.4.0._
-     */
-    function mod(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
-        require(b != 0, errorMessage);
-        return a % b;
     }
 }
 
@@ -173,27 +52,27 @@ contract MolGamma { // Γ - mv - NFT - mkt - γ
     mapping(address => uint256) public balanceOf;
     mapping(uint256 => address) public getApproved;
     mapping(uint256 => address) public ownerOf;
-    mapping(uint256 => uint8) public didPrimarySale; // Primary sale
+    mapping(uint256 => uint8) public didPrimarySale; // 0 = !(primary sale) 1 = primary sale
+    mapping(uint256 => uint256) public remixOf; // array for storing remix references
     mapping(uint256 => uint256) public tokenByIndex;
     mapping(uint256 => string) public tokenURI;
-    mapping(uint256 => uint256) public remixOf; // Array for storing remix references
     mapping(uint256 => Sale) public sale;
     mapping(bytes4 => bool) public supportsInterface; // eip-165
-    mapping(address => mapping(address => bool)) public isApprovedForAll;
-    mapping(uint256 => uint256[]) public ownersRoyaltiesByTokenId; // ownerIndex[tokenId][Owner struct]
     mapping(uint256 => address payable[]) public ownersByTokenId; // ownersPerTokenId[tokenId][owner address]
+    mapping(uint256 => uint256[]) public ownersRoyaltiesByTokenId; // ownerIndex[tokenId][Owner struct]
+    mapping(address => mapping(address => bool)) public isApprovedForAll;
     mapping(address => mapping(uint256 => uint256)) public tokenOfOwnerByIndex;
     event Approval(address indexed approver, address indexed spender, uint256 indexed tokenId);
     event ApprovalForAll(address indexed owner, address indexed operator, bool approved);
-    event MolFeesUpdated(uint256 indexed _molFees);
     event MolBankUpdated(address indexed _molBank);
+    event MolFeesUpdated(uint256 indexed _molFees);
     event MolUpdated(address indexed _mol);
     event Remixed(uint256 indexed remixOfTokenId);
     event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
     event UpdateSale(uint256 indexed ethPrice, uint256 indexed tokenId, uint8 forSale);
     struct Sale {
-        uint256 ethPrice;
         uint8 forSale; // 0 = not on sale, 1 = on sale
+        uint256 ethPrice;
     }
     constructor (string memory _name, string memory _symbol) public {
         supportsInterface[0x80ac58cd] = true; // ERC721
@@ -207,8 +86,30 @@ contract MolGamma { // Γ - mv - NFT - mkt - γ
         getApproved[tokenId] = spender;
         emit Approval(msg.sender, spender, tokenId);
     }
+    function distributeRoyalties(uint256 _tokenId, uint256 _ethPrice) private returns (uint256){
+        require(ownersByTokenId[_tokenId].length == ownersRoyaltiesByTokenId[_tokenId].length, "!ownersByTokenId/ownerRoyaltiesByTokenId");
+        uint256 totalPayout = _ethPrice.div(100);
+        uint256 royaltyPayout;
+
+        for (uint256 i = 0; i < ownersByTokenId[_tokenId].length; i++) {
+            uint256 eachPayout;
+            eachPayout = totalPayout.mul(ownersRoyaltiesByTokenId[_tokenId][i]);
+            royaltyPayout += eachPayout;
+            (bool success, ) = ownersByTokenId[_tokenId][i].call.value(eachPayout)("");
+            require(success, "!transfer");
+        }
+        return royaltyPayout;
+    }
+    function getAllTokenURI() public view returns (string[] memory){
+        string[] memory allTokenURI = new string[](totalSupply);
+        for (uint i = 0; i < totalSupply; i++) {
+            allTokenURI[i] = tokenURI[i.add(1)];
+        }
+        return allTokenURI;
+    }
     function mint(uint256 ethPrice, uint256 remixOfTokenId, string calldata _tokenURI, uint8 forSale) external {
         totalSupply++;
+        require(forSale <= 1, "!forSale value");
         require(totalSupply <= GAMMA_MAX, "maxed");
         uint256 tokenId = totalSupply;
         balanceOf[msg.sender]++;
@@ -230,9 +131,7 @@ contract MolGamma { // Γ - mv - NFT - mkt - γ
         require(msg.value == sale[tokenId].ethPrice, "!ethPrice");
         require(msg.sender != ownerOf[tokenId], "owner"); // Communal ownership
         require(sale[tokenId].forSale == 1, "!forSale");
-
         address owner = ownerOf[tokenId];
-
         // Distribute ethPrice
         if (didPrimarySale[tokenId] == 0) {
             (bool success, ) = owner.call.value(msg.value)("");
@@ -247,7 +146,6 @@ contract MolGamma { // Γ - mv - NFT - mkt - γ
             (success, ) = owner.call.value(ownerCut)("");
             require(success, "!transfer");
         }
-
         _transfer(owner, msg.sender, tokenId);
         ownersByTokenId[tokenId].push(msg.sender); // push minter to owners registry per token Id
         ownersRoyaltiesByTokenId[tokenId].push(ownersRoyaltiesByTokenId[tokenId][ownersRoyaltiesByTokenId[tokenId].length.sub(1)].sub(1)); // push decayed royalties % to royalties registry per token Id
@@ -282,31 +180,10 @@ contract MolGamma { // Γ - mv - NFT - mkt - γ
         _transfer(from, to, tokenId);
     }
     function updateSale(uint256 ethPrice, uint256 tokenId, uint8 forSale) payable external {
-        require(msg.sender == ownerOf[tokenId], "!owner"); // Communal ownership
+        require(msg.sender == ownerOf[tokenId], "!owner");
         sale[tokenId].ethPrice = ethPrice;
         sale[tokenId].forSale = forSale;
         emit UpdateSale(ethPrice, tokenId, forSale);
-    }
-    function distributeRoyalties(uint256 _tokenId, uint256 _ethPrice) private returns (uint256){
-        require(ownersByTokenId[_tokenId].length == ownersRoyaltiesByTokenId[_tokenId].length, "!ownersByTokenId/ownerRoyaltiesByTokenId");
-        uint256 totalPayout = _ethPrice.div(100);
-        uint256 royaltyPayout;
-
-        for (uint256 i = 0; i < ownersByTokenId[_tokenId].length; i++) {
-            uint256 eachPayout;
-            eachPayout = totalPayout.mul(ownersRoyaltiesByTokenId[_tokenId][i]);
-            royaltyPayout += eachPayout;
-            (bool success, ) = ownersByTokenId[_tokenId][i].call.value(eachPayout)("");
-            require(success, "!transfer");
-        }
-        return royaltyPayout;
-    }
-    function getAllTokenURI() public view returns (string[] memory){
-        string[] memory ret = new string[](totalSupply);
-        for (uint i = 0; i < totalSupply; i++) {
-            ret[i] = tokenURI[i.add(1)];
-        }
-        return ret;
     }
 
     /***************
@@ -316,19 +193,19 @@ contract MolGamma { // Γ - mv - NFT - mkt - γ
         require(msg.sender == mol, "caller not lexDAO");
         _;
     }
-    function updateMolFees(uint256 _molFee) public onlyMol {
-        molFee = _molFee;
-        emit MolFeesUpdated(molFee);
-    }
-    function updateMolBank(address payable _molBank) public onlyMol {
-        molBank = _molBank;
-        emit MolBankUpdated(molBank);
+    function molTransfer(address to, uint256 tokenId) public onlyMol {
+        _transfer(ownerOf[tokenId], to, tokenId);
     }
     function updateMol(address payable _mol) public onlyMol {
         mol = _mol;
         emit MolUpdated(mol);
     }
-    function molTransfer(address to, uint256 tokenId) public onlyMol {
-        _transfer(ownerOf[tokenId], to, tokenId);
+    function updateMolBank(address payable _molBank) public onlyMol {
+        molBank = _molBank;
+        emit MolBankUpdated(molBank);
+    }
+    function updateMolFees(uint256 _molFee) public onlyMol {
+        molFee = _molFee;
+        emit MolFeesUpdated(molFee);
     }
 }
