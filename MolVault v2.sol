fe2b56e78106d68fe03cd074a70510ea6f881b2b
audsssy <stamfordh@hotmail.com>
2021-02-24 13:11:35 -0500

    Update MolVault v2.sol
    Clean up

--- MolVault v2.sol
+++ MolVault v2.sol
@@ -57,6 +57,127 @@ library Utilities {
 	}
 }
 
+contract GAMMA { // Γ - mv - NFT - mkt - γ
+    address payable public dao = 0x057e820D740D5AAaFfa3c6De08C5c98d990dB00d;
+    uint256 public constant GAMMA_MAX = 5772156649015328606065120900824024310421;
+    uint256 public totalSupply;
+    string public name = "GAMMA";
+    string public symbol = "GAMMA";
+    mapping(address => uint256) public balanceOf;
+    mapping(uint256 => address) public getApproved;
+    mapping(uint256 => address) public ownerOf;
+    mapping(uint256 => uint256) public tokenByIndex;
+    mapping(uint256 => string) public tokenURI;
+    mapping(uint256 => Sale) public sale;
+    mapping(bytes4 => bool) public supportsInterface; // eip-165 
+    mapping(address => mapping(address => bool)) public isApprovedForAll;
+    mapping(address => mapping(uint256 => uint256)) public tokenOfOwnerByIndex;
+    event Approval(address indexed approver, address indexed spender, uint256 indexed tokenId);
+    event ApprovalForAll(address indexed owner, address indexed operator, bool approved);
+    event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
+    event UpdateSale(uint256 indexed ethPrice, uint256 indexed tokenId, uint8 forSale);
+    struct Sale {
+        uint256 ethPrice;
+        uint8 forSale;
+    }
+    constructor () public {
+        supportsInterface[0x80ac58cd] = true; // ERC721 
+        supportsInterface[0x5b5e139f] = true; // METADATA
+        supportsInterface[0x780e9d63] = true; // ENUMERABLE
+    }
+    function approve(address spender, uint256 tokenId) external {
+        require(msg.sender == ownerOf[tokenId] || isApprovedForAll[ownerOf[tokenId]][msg.sender], "!owner/operator");
+        getApproved[tokenId] = spender;
+        emit Approval(msg.sender, spender, tokenId); 
+    }
+    function mint(uint256 ethPrice, string calldata _tokenURI, uint8 forSale) external { 
+        totalSupply++;
+        require(totalSupply <= GAMMA_MAX, "maxed");
+        uint256 tokenId = totalSupply;
+        balanceOf[msg.sender]++;
+        ownerOf[tokenId] = msg.sender;
+        tokenByIndex[tokenId - 1] = tokenId;
+        tokenURI[tokenId] = _tokenURI;
+        sale[tokenId].ethPrice = ethPrice;
+        sale[tokenId].forSale = forSale;
+        tokenOfOwnerByIndex[msg.sender][tokenId - 1] = tokenId;
+        emit Transfer(address(0), msg.sender, tokenId); 
+        emit UpdateSale(ethPrice, tokenId, forSale);
+    }
+    function purchase(uint256 tokenId) payable external {
+        require(msg.value == sale[tokenId].ethPrice, "!ethPrice");
+        require(sale[tokenId].forSale == 1, "!forSale");
+        address owner = ownerOf[tokenId];
+        (bool success, ) = owner.call{value: msg.value}("");
+        require(success, "!transfer");
+        _transfer(owner, msg.sender, tokenId);
+    }
+    function setApprovalForAll(address operator, bool approved) external {
+        isApprovedForAll[msg.sender][operator] = approved;
+        emit ApprovalForAll(msg.sender, operator, approved);
+    }
+    function _transfer(address from, address to, uint256 tokenId) internal {
+        balanceOf[from]--; 
+        balanceOf[to]++; 
+        getApproved[tokenId] = address(0);
+        ownerOf[tokenId] = to;
+        sale[tokenId].forSale = 0;
+        tokenOfOwnerByIndex[from][tokenId - 1] = 0;
+        tokenOfOwnerByIndex[to][tokenId - 1] = tokenId;
+        emit Transfer(from, to, tokenId); 
+    }
+    function transfer(address to, uint256 tokenId) external {
+        require(msg.sender == ownerOf[tokenId], "!owner");
+        _transfer(msg.sender, to, tokenId);
+    }
+    function transferBatch(address[] calldata to, uint256[] calldata tokenId) external {
+        require(to.length == tokenId.length, "!to/tokenId");
+        for (uint256 i = 0; i < to.length; i++) {
+            require(msg.sender == ownerOf[tokenId[i]], "!owner");
+            _transfer(msg.sender, to[i], tokenId[i]);
+        }
+    }
+    function transferFrom(address from, address to, uint256 tokenId) external {
+        require(msg.sender == ownerOf[tokenId] || getApproved[tokenId] == msg.sender || isApprovedForAll[ownerOf[tokenId]][msg.sender], "!owner/spender/operator");
+        _transfer(from, to, tokenId);
+    }
+    function updateDao(address payable _dao) external {
+        require(msg.sender == dao, "!dao");
+        dao = _dao;
+    }
+    function updateSale(uint256 ethPrice, uint256 tokenId, uint8 forSale) payable external {
+        require(msg.sender == ownerOf[tokenId], "!owner");
+        sale[tokenId].ethPrice = ethPrice;
+        sale[tokenId].forSale = forSale;
+        (bool success, ) = dao.call{value: msg.value}("");
+        require(success, "!transfer");
+        emit UpdateSale(ethPrice, tokenId, forSale);
+    }
+}
+
+contract LiteToken { // minimal viable erc20 token with common extensions, such as burn, cap, mint, pauseable, admin functions
+    address public owner;
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    uint256 public totalSupply;
+    uint256 public totalSupplyCap;
+    bool public transferable; 
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+    event Transfer(address indexed from, address indexed to, uint256 amount);
+    mapping(address => mapping(address => uint256)) public allowance;
+    mapping(address => uint256) public balanceOf;
+    constructor(string memory _name, string memory _symbol, uint8 _decimals, address _owner, uint256 _initialSupply, uint256 _totalSupplyCap, bool _transferable) public {require(_initialSupply <= _totalSupplyCap, "capped");
+        name = _name; symbol = _symbol; decimals = _decimals; owner = _owner; totalSupply = _initialSupply; totalSupplyCap = _totalSupplyCap; transferable = _transferable; balanceOf[owner] = totalSupply; emit Transfer(address(0), owner, totalSupply);}
+    function approve(address spender, uint256 amount) external returns (bool) {require(amount == 0 || allowance[msg.sender][spender] == 0); allowance[msg.sender][spender] = amount; emit Approval(msg.sender, spender, amount); return true;}
+    function burn(uint256 amount) external {balanceOf[msg.sender] = balanceOf[msg.sender] - amount; totalSupply = totalSupply - amount; emit Transfer(msg.sender, address(0), amount);}
+    function mint(address recipient, uint256 amount) external {require(msg.sender == owner, "!owner"); require(totalSupply + amount <= totalSupplyCap, "capped"); balanceOf[recipient] = balanceOf[recipient] + amount; totalSupply = totalSupply + amount; emit Transfer(address(0), recipient, amount);}
+    function transfer(address recipient, uint256 amount) external returns (bool) {require(transferable == true); balanceOf[msg.sender] = balanceOf[msg.sender] - amount; balanceOf[recipient] = balanceOf[recipient] + amount; emit Transfer(msg.sender, recipient, amount); return true;}
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool) {require(transferable == true); balanceOf[sender] = balanceOf[sender] - amount; balanceOf[recipient] = balanceOf[recipient] + amount; allowance[sender][msg.sender] = allowance[sender][msg.sender] - amount; emit Transfer(sender, recipient, amount); return true;}
+    function transferOwner(address _owner) external {require(msg.sender == owner, "!owner"); owner = _owner;}
+    function updateTransferability(bool _transferable) external {require(msg.sender == owner, "!owner"); transferable = _transferable;}
+}
+
 contract MolVault {
     using SafeMath for uint256;
     address vault = address(this);
@@ -372,6 +493,7 @@ contract MolVault {
 	    airdrop = amount;
 	}
 	
+	// consider removing this
 	function transferVaultShares(address payable _recipient, uint256 _amount) public onlyOwners{
 	    vaultShares.transferFrom(vault, _recipient, _amount);
 	}
@@ -382,125 +504,4 @@ contract MolVault {
 	}
 	
     receive() external payable {  require(msg.data.length ==0); }
-}
-
-contract GAMMA { // Γ - mv - NFT - mkt - γ
-    address payable public dao = 0x057e820D740D5AAaFfa3c6De08C5c98d990dB00d;
-    uint256 public constant GAMMA_MAX = 5772156649015328606065120900824024310421;
-    uint256 public totalSupply;
-    string public name = "GAMMA";
-    string public symbol = "GAMMA";
-    mapping(address => uint256) public balanceOf;
-    mapping(uint256 => address) public getApproved;
-    mapping(uint256 => address) public ownerOf;
-    mapping(uint256 => uint256) public tokenByIndex;
-    mapping(uint256 => string) public tokenURI;
-    mapping(uint256 => Sale) public sale;
-    mapping(bytes4 => bool) public supportsInterface; // eip-165 
-    mapping(address => mapping(address => bool)) public isApprovedForAll;
-    mapping(address => mapping(uint256 => uint256)) public tokenOfOwnerByIndex;
-    event Approval(address indexed approver, address indexed spender, uint256 indexed tokenId);
-    event ApprovalForAll(address indexed owner, address indexed operator, bool approved);
-    event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
-    event UpdateSale(uint256 indexed ethPrice, uint256 indexed tokenId, uint8 forSale);
-    struct Sale {
-        uint256 ethPrice;
-        uint8 forSale;
-    }
-    constructor () public {
-        supportsInterface[0x80ac58cd] = true; // ERC721 
-        supportsInterface[0x5b5e139f] = true; // METADATA
-        supportsInterface[0x780e9d63] = true; // ENUMERABLE
-    }
-    function approve(address spender, uint256 tokenId) external {
-        require(msg.sender == ownerOf[tokenId] || isApprovedForAll[ownerOf[tokenId]][msg.sender], "!owner/operator");
-        getApproved[tokenId] = spender;
-        emit Approval(msg.sender, spender, tokenId); 
-    }
-    function mint(uint256 ethPrice, string calldata _tokenURI, uint8 forSale) external { 
-        totalSupply++;
-        require(totalSupply <= GAMMA_MAX, "maxed");
-        uint256 tokenId = totalSupply;
-        balanceOf[msg.sender]++;
-        ownerOf[tokenId] = msg.sender;
-        tokenByIndex[tokenId - 1] = tokenId;
-        tokenURI[tokenId] = _tokenURI;
-        sale[tokenId].ethPrice = ethPrice;
-        sale[tokenId].forSale = forSale;
-        tokenOfOwnerByIndex[msg.sender][tokenId - 1] = tokenId;
-        emit Transfer(address(0), msg.sender, tokenId); 
-        emit UpdateSale(ethPrice, tokenId, forSale);
-    }
-    function purchase(uint256 tokenId) payable external {
-        require(msg.value == sale[tokenId].ethPrice, "!ethPrice");
-        require(sale[tokenId].forSale == 1, "!forSale");
-        address owner = ownerOf[tokenId];
-        (bool success, ) = owner.call{value: msg.value}("");
-        require(success, "!transfer");
-        _transfer(owner, msg.sender, tokenId);
-    }
-    function setApprovalForAll(address operator, bool approved) external {
-        isApprovedForAll[msg.sender][operator] = approved;
-        emit ApprovalForAll(msg.sender, operator, approved);
-    }
-    function _transfer(address from, address to, uint256 tokenId) internal {
-        balanceOf[from]--; 
-        balanceOf[to]++; 
-        getApproved[tokenId] = address(0);
-        ownerOf[tokenId] = to;
-        sale[tokenId].forSale = 0;
-        tokenOfOwnerByIndex[from][tokenId - 1] = 0;
-        tokenOfOwnerByIndex[to][tokenId - 1] = tokenId;
-        emit Transfer(from, to, tokenId); 
-    }
-    function transfer(address to, uint256 tokenId) external {
-        require(msg.sender == ownerOf[tokenId], "!owner");
-        _transfer(msg.sender, to, tokenId);
-    }
-    function transferBatch(address[] calldata to, uint256[] calldata tokenId) external {
-        require(to.length == tokenId.length, "!to/tokenId");
-        for (uint256 i = 0; i < to.length; i++) {
-            require(msg.sender == ownerOf[tokenId[i]], "!owner");
-            _transfer(msg.sender, to[i], tokenId[i]);
-        }
-    }
-    function transferFrom(address from, address to, uint256 tokenId) external {
-        require(msg.sender == ownerOf[tokenId] || getApproved[tokenId] == msg.sender || isApprovedForAll[ownerOf[tokenId]][msg.sender], "!owner/spender/operator");
-        _transfer(from, to, tokenId);
-    }
-    function updateDao(address payable _dao) external {
-        require(msg.sender == dao, "!dao");
-        dao = _dao;
-    }
-    function updateSale(uint256 ethPrice, uint256 tokenId, uint8 forSale) payable external {
-        require(msg.sender == ownerOf[tokenId], "!owner");
-        sale[tokenId].ethPrice = ethPrice;
-        sale[tokenId].forSale = forSale;
-        (bool success, ) = dao.call{value: msg.value}("");
-        require(success, "!transfer");
-        emit UpdateSale(ethPrice, tokenId, forSale);
-    }
-}
-
-contract LiteToken { // minimal viable erc20 token with common extensions, such as burn, cap, mint, pauseable, admin functions
-    address public owner;
-    string public name;
-    string public symbol;
-    uint8 public decimals;
-    uint256 public totalSupply;
-    uint256 public totalSupplyCap;
-    bool public transferable; 
-    event Approval(address indexed owner, address indexed spender, uint256 value);
-    event Transfer(address indexed from, address indexed to, uint256 amount);
-    mapping(address => mapping(address => uint256)) public allowance;
-    mapping(address => uint256) public balanceOf;
-    constructor(string memory _name, string memory _symbol, uint8 _decimals, address _owner, uint256 _initialSupply, uint256 _totalSupplyCap, bool _transferable) public {require(_initialSupply <= _totalSupplyCap, "capped");
-        name = _name; symbol = _symbol; decimals = _decimals; owner = _owner; totalSupply = _initialSupply; totalSupplyCap = _totalSupplyCap; transferable = _transferable; balanceOf[owner] = totalSupply; emit Transfer(address(0), owner, totalSupply);}
-    function approve(address spender, uint256 amount) external returns (bool) {require(amount == 0 || allowance[msg.sender][spender] == 0); allowance[msg.sender][spender] = amount; emit Approval(msg.sender, spender, amount); return true;}
-    function burn(uint256 amount) external {balanceOf[msg.sender] = balanceOf[msg.sender] - amount; totalSupply = totalSupply - amount; emit Transfer(msg.sender, address(0), amount);}
-    function mint(address recipient, uint256 amount) external {require(msg.sender == owner, "!owner"); require(totalSupply + amount <= totalSupplyCap, "capped"); balanceOf[recipient] = balanceOf[recipient] + amount; totalSupply = totalSupply + amount; emit Transfer(address(0), recipient, amount);}
-    function transfer(address recipient, uint256 amount) external returns (bool) {require(transferable == true); balanceOf[msg.sender] = balanceOf[msg.sender] - amount; balanceOf[recipient] = balanceOf[recipient] + amount; emit Transfer(msg.sender, recipient, amount); return true;}
-    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool) {require(transferable == true); balanceOf[sender] = balanceOf[sender] - amount; balanceOf[recipient] = balanceOf[recipient] + amount; allowance[sender][msg.sender] = allowance[sender][msg.sender] - amount; emit Transfer(sender, recipient, amount); return true;}
-    function transferOwner(address _owner) external {require(msg.sender == owner, "!owner"); owner = _owner;}
-    function updateTransferability(bool _transferable) external {require(msg.sender == owner, "!owner"); transferable = _transferable;}
 }
